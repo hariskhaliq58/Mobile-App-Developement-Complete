7c03a8ff5123ffd9e1cb5dbf26a4f97863ddcec5
Muhammad Haris <147323194+hariskhaliq58@users.noreply.github.com>
2024-05-27 18:53:56 +0500

    Update firebase.js

--- src/screens/firebase.js
+++ src/screens/firebase.js
@@ -7,13 +7,13 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBpXUupzh3unx2DV7e7ib2PQrUAIvmJ4lM",
-  authDomain: "authentication-4ffe3.firebaseapp.com",
-  projectId: "authentication-4ffe3",
-  storageBucket: "authentication-4ffe3.appspot.com",
-  messagingSenderId: "342136340656",
-  appId: "1:342136340656:web:8f6af6b470da4e7e4afb75",
-  measurementId: "G-5GLJD4SRBR"
+  apiKey: " ",
+  authDomain: " ",
+  projectId: " ",
+  storageBucket: " ",
+  messagingSenderId: " ",
+  appId: " ",
+  measurementId: " "
 };
 
 const app = firebase.initializeApp(firebaseConfig);
